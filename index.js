4e0d912bd89ae23d87c2e4eeaa7e886a096af7f0
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2020-11-23 18:45:45 -0500

    succesfully connected to server

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const repoContext = require("./repository/repository-wrapper.js");
 const express = require('express');
 
 const app = express();
 
 app.listen(3000, function () {
-    console.log('Server started. Listenong on port 3000.');
+    console.log('Server started. Listening on port 3000.');
 });
