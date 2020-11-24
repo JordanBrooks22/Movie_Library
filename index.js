f9d1068e1bbcff8499e819fb1b16ea3bee244717
JordanBrooks22 <73143990+JordanBrooks22@users.noreply.github.com>
2020-11-23 19:34:36 -0500

    able to get a specific movie title by id

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const app = express();
 app.listen(3000, function () {
     console.log('Server started. Listening on port 3000.');
 });
+app.get("/api/movies", (req, res) => {
+    let movies = repoContext.movies.findAllMovies();
+    res.send(movies);
+   });
+app.get("/api/movies/:id", (req, res) => {
+    let id = req.params.id;
+    let movies = repoContext.movies.findMovieById(id);
+    res.send(movies);
+   });
+   
